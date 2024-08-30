2b79d1a0e02c0110a0b44b64829866be90bbc31d
Dhruvit Garathiya <138596746+dhruvitgarathiya@users.noreply.github.com>
2024-08-30 09:52:52 +0530

    dark mode feature added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'; // assuming you have Bootstrap installed
-import TextForm from './Components/TextForm/TextForm';
+ import TextForm from './Components/TextForm/TextForm';
 import Navbar from './Components/Navbar/Navbar';
+import About from './Components/About/About';
 
 function App() {
   return (
     <>
   <Navbar title="textutill"  aboutText="aboutus"/>
-  <div className='container my-3'> 
-    < TextForm  heading="Enter the text to analyze"/>
+  <div className='container my-3'>
+    
+  <TextForm  heading="Enter the text to analyze"/> 
+   
+    <About />
   </div> 
  
     </> 
